a65cfe30d43cd5757b57ea22457736923399b56c
Christoph Wasylewski <e1226587@student.tuwien.ac.at>
2017-01-04 11:28:20 +0100

    zu viele ähnliche Tests: hinzufügen von tests um alle Exceptions abzudecken umbenennen der Tests um die Lesbarkeit zu erhöhen Folgende Tests sind nun vorhanden:
    testRentalValidationRentalIsNull();
    testRentalValidationOnlyMovieIsSet();
    testRentalValidationOnlyCostumerIsSet();
    testRentalValidationNothingIsSet();
    testRentalValidationCustomerAndMovieIsSet();
    testRentalValidationAllSetBadMovieYoungCustomer();
    testRentalValidationAllSetGoodMovieYoungCustomer();
    testRentalValidationAllSetBadMovieOldCustomer();
    ____
    erstellen der Methode:
    @Before
     public void beforeEachTest() {
    
     }
    
     um sicherzustellen, dass vor jedem Test alls Vorbedingungen erfüllt sind und keine Seiteneffekte enstehen können
     ____
    
     erstellen eines neuen Customer : oldCustomer um die Alterfreigabe für "badMovies" zu überprüfen
     ____
    
     Die Ausgabe um den Namen des Tests erweitert, um die Lesbarkeit zu optimieren und die Testfälle zu überprüfen

--- test/OldTests.java
+++ test/OldTests.java
@@ -17,7 +17,7 @@ import static org.junit.Assert.assertTrue;
 public class OldTests {
 
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd. MM. yyyy");
-    private static Customer youngCustomer;
+    private static Customer youngCustomer, oldCustomer;
     private static Movie badMovie, goodMovie;
     private static Rental rental;
     private static RentalController rentalController;
@@ -25,63 +25,113 @@ public class OldTests {
     @BeforeClass
     public static void setUp() {
         youngCustomer = new Customer();
+        oldCustomer = new Customer();
         badMovie = new Movie();
         goodMovie = new Movie();
         rental = new Rental();
         rentalController = new RentalController();
     }
     @Before
-    public static void beforeEachTest() {
+    public void beforeEachTest() {
         //yountCustomer
        initialYoungCustomer();
         //badMovie
        initalBadMovie();
         //goodMovie
        initialGoodMovie();
+       //oldCustomer
+        initialOldCustomer();
+       rental= new Rental();
+       rentalController=new RentalController();
+
+    }
+    @After
+    public void afterEachTest(){
+        System.out.println("");
     }
 
+    /**
+     * rental is null --> ValidationException("Rental has to be not null");
+     */
     @Test
-    public void testRentalValidationGoodMovie() {
-        // movie set
+    public void testRentalValidationRentalIsNull() {
+        System.out.println("testRentalValidationRentalIsNull(): ");
+        rental =null;
+        testvalidateRental(rental);
+    }
+
+    /**
+     * Only movie is set --> ValidationException("Kunde muss gesetzt sein.");
+     */
+    @Test
+    public void testRentalValidationOnlyMovieIsSet() {
+        System.out.println("testRentalValidationOnlyMovieIsSet(): ");
         rental.setMovie(goodMovie);
         testvalidateRental(rental);
     }
-
+    /**
+     * Only customer is set --> ValidationException("Film muss gesetzt sein.");
+     */
     @Test
-    public void testRentalValidationNull() {
-        // nothing set
-        rental.setMovie(null);
-        testvalidateRental(rental);
-    }
-
-    @Test
-    public void testRentalValidationYoungCustomer() {
-        // customer set
+    public void testRentalValidationOnlyCostumerIsSet() {
+        System.out.println("testRentalValidationOnlyCostumerIsSet(): ");
         rental.setCustomer(youngCustomer);
         testvalidateRental(rental);
     }
+
+    /**
+     * Nothing is set --> ValidationException("Kunde muss gesetzt sein.");
+     */
     @Test
-    public void testRentalValidationYoungCustomerAndBadMovie() {
-        // customer and bad movie set
-        rental.setMovie(badMovie);
+    public void testRentalValidationNothingIsSet() {
+        System.out.println("testRentalValidationNothingIsSet(): ");
         testvalidateRental(rental);
     }
+
+    /**
+     * Customer and Movie is set --> ValidationException("Verleihtag muss gesetzt sein.");
+     */
     @Test
-    public void testRentalValidationYoungCustomerAndBadMovieLentDateSet() {
-        // customer, bad movie and lent date set
-        rental.setDateLent(LocalDate.now());
-        testvalidateRental(rental);
-    }
-    @Test
-    public void testRentalValidationYoungCustomerAndGoodMovie() {
-        // customer and good movie set
-        rental.setDateLent(null);
+    public void testRentalValidationCustomerAndMovieIsSet() {
+        System.out.println("testRentalValidationCustomerAndMovieIsSet(): ");
+        rental.setCustomer(youngCustomer);
         rental.setMovie(goodMovie);
         testvalidateRental(rental);
     }
+
+    /**
+     * All is set but young customer and bad movie --> ValidationException("Der Kunde erfüllt die Altersfreigabe nicht! (FSK 18)");
+     */
     @Test
-    public void testRentalValidationYoungCustomerAndGoodMovieLentDateSet(){
-        // customer, good movie and lent date set
+    public void testRentalValidationAllSetBadMovieYoungCustomer() {
+        System.out.println("testRentalValidationAllSetBadMovieYoungCustomer(): ");
+
+        rental.setMovie(badMovie);
+        rental.setCustomer(youngCustomer);
+        rental.setDateLent(LocalDate.now());
+        testvalidateRental(rental);
+    }
+
+    /**
+     * All is set and young customer and good movie --> passed
+     */
+    @Test
+    public void testRentalValidationAllSetGoodMovieYoungCustomer() {
+        System.out.println("testRentalValidationAllSetGoodMovieYoungCustomer(): ");
+        rental.setMovie(goodMovie);
+        rental.setCustomer(youngCustomer);
+        rental.setDateLent(LocalDate.now());
+        testvalidateRental(rental);
+    }
+
+    /**
+     * All is set but old customer and bad movie --> passed
+     */
+    @Test
+    public void testRentalValidationAllSetBadMovieOldCustomer() {
+        System.out.println("testRentalValidationAllSetBadMovieOldCustomer(): ");
+        rental.setMovie(badMovie);
+        rental.setCustomer(oldCustomer);
         rental.setDateLent(LocalDate.now());
         testvalidateRental(rental);
     }
@@ -91,12 +141,12 @@ public class OldTests {
      * die Methode testvalidateRental(Rental rental) erstellt
      */
     private void testvalidateRental(Rental rental){
-        try {
-            rentalController.validateRental(rental);
-            assertTrue(false);
-        } catch (ValidationException e) {
-            assertTrue(true);
-        }
+            try {
+                rentalController.validateRental(rental);
+                System.out.println("Test passed");
+            } catch (ValidationException e) {
+                System.out.println(e);
+            }
     }
     /**
      * Auslagerung der initialisierung
@@ -114,6 +164,20 @@ public class OldTests {
         youngCustomer.setPatron(true);
         youngCustomer.setPhoto(null);
         youngCustomer.setVideopoints(2);
+    }
+    private static void initialOldCustomer(){
+        oldCustomer.setId(1L);
+        oldCustomer.setFirstName("Linda");
+        oldCustomer.setLastName("Mayer");
+        oldCustomer.setEmail("Lucy2001@hotmail.com");
+        oldCustomer.setPhone("+43 555 61 61");
+        oldCustomer.setBirthday(LocalDate.parse("01. 04. 1970", DATE_TIME_FORMATTER));
+        oldCustomer.setAddress("Berggasse 11");
+        oldCustomer.setZipCode("3002");
+        oldCustomer.setCity("Purkersdorf");
+        oldCustomer.setPatron(true);
+        oldCustomer.setPhoto(null);
+        oldCustomer.setVideopoints(2);
     }
     private static void initalBadMovie(){
         badMovie.setId(1L);
